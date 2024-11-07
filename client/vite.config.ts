004ed6ea2810aa97ccb74d2153b308c72c539738
Lil-Ran <lilran4869@qq.com>
2024-11-07 16:03:24 +0800

    wip: vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,54 @@
-import { defineConfig } from 'vite'
+import eslintPlugin from '@nabla/vite-plugin-eslint'
 import react from '@vitejs/plugin-react'
+import process from 'process'
+import { defineConfig, loadEnv } from 'vite'
+import Pages from 'vite-plugin-pages'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  const TARGET = env.VITE_BACKEND_URL ?? 'http://localhost:55000'
+
+  return {
+    server: {
+      port: 63000,
+      proxy: {
+        '/api': TARGET,
+        '/assets': TARGET,
+      },
+    },
+    preview: {
+      port: 64000,
+    },
+    build: {
+      assetsDir: 'static',
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'static/[hash].js',
+          assetFileNames: 'static/[hash].[ext]',
+          entryFileNames: 'static/[name].[hash].js',
+          compact: true,
+        },
+      },
+    },
+    html: {
+      cspNonce: '%nonce%',
+    },
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      eslintPlugin(), // only for development
+      Pages({
+        dirs: [
+          {
+            dir: './src/pages',
+            baseRoute: '',
+            filePattern: '**/*.tsx',
+          },
+        ],
+      }),
+    ],
+  }
 })
