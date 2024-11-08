6768bc1c69d951892622d89264f3895a70bc068e
Lil-Ran <lilran4869@qq.com>
2024-11-08 18:51:49 +0800

    wip: code style and linters

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,13 +1,19 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import oxlint from 'eslint-plugin-oxlint'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from "eslint-config-prettier"
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      oxlint.configs['flat/recommended'],
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -23,6 +29,19 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        {
+          "selector": "variable",
+          "modifiers": ["const", "global"],
+          "format": ["PascalCase", "camelCase", "UPPER_CASE"],
+        },
+        {
+          "selector": "variable",
+          "format": ["PascalCase", "camelCase"],
+        }
+      ],
     },
   },
+  eslintConfigPrettier,
 )
