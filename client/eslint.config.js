0dc275e1daed2b8bf6e89c2be744518c0373b29f
Lil-Ran <lilran4869@qq.com>
2024-11-12 15:46:51 +0800

    wip: add mantine

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,47 +1,57 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import oxlint from 'eslint-plugin-oxlint'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import eslintConfigPrettier from "eslint-config-prettier"
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
       oxlint.configs['flat/recommended'],
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/naming-convention': [
         'warn',
-        { allowConstantExport: true },
-      ],
-      "@typescript-eslint/naming-convention": [
-        "warn",
         {
-          "selector": "variable",
-          "modifiers": ["const", "global"],
-          "format": ["PascalCase", "camelCase", "UPPER_CASE"],
+          selector: 'variable',
+          modifiers: ['const', 'global'],
+          format: ['PascalCase', 'camelCase', 'UPPER_CASE'],
         },
         {
-          "selector": "variable",
-          "format": ["PascalCase", "camelCase"],
-        }
+          selector: 'variable',
+          format: ['PascalCase', 'camelCase'],
+        },
       ],
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
-  eslintConfigPrettier,
+  eslintConfigPrettier
 )
